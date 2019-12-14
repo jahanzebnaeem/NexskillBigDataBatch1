972404b8cd9eeeb9c652d6d5ce0d104b12933839
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2019-12-14 21:38:57 +0500

    Lecture 7
    Added the conditional statement file and renamed pythonpractice.py file to python-intro.py

---
+++ python-con.py
@@ -0,0 +1,19 @@
+# These are variables
+myName = "Abdullah"
+myAge = 24
+
+""" Hi how are you doing
+this is a multiline comment
+"""
+
+if myAge != 24:
+    print("You are not {}".format(myName))
+else:
+    print("You are {}".format(myName))
+
+for i in range(1, 10):
+    if i != 2:
+        print(i)
+        break
+
+#print(opt1%opt2)
