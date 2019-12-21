9f606fb6de737010772bcd077a3e6019c61c6940
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2019-12-21 15:18:36 +0500

    Lecture 7
    Understanding of lists and functions

---
+++ python-list.py
@@ -0,0 +1,18 @@
+"""
+numberCount = [1, 2, 5, 7, 25]
+numberCount2 = [3, 4, 6, 8, 9]
+#print(numberCount[-3:])
+print (numberCount + numberCount2)
+
+letter = ['a', 'b', 'c', 'd']
+print(len(letter))
+
+i = 1
+while i < 5:
+    print(i)
+    i += 1
+"""
+def cal(a, b):
+    return a+b
+
+print(cal(1, 2))
